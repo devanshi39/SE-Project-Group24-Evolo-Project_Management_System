016f8f68c0a78f6dc5cee31faf92caa99d6149ea
indranl1 <banerjeeindranil350@gmail.com>
2022-10-09 14:43:05 -0400

    updated test configurations for build 3

--- tests/calculateTest.php
+++ tests/calculateTest.php
@@ -4,13 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class calculateTest extends TestCase {
 	public function testadd() {
-		$calculate = new code\calculate;
+		$calculate = new code/calculate;
 		$calculate->setOperands([5,20]);
 		$this->assertEquals(25, $calculate->add());
-
 }
 	public function testsubtract() {
-		$calculate = new code\calculate;
+		$calculate = new code\\calculate;
 		$calculate->setOperands([50,49]);
 		$this->assertEquals(1,$calculate->subtract());
 	}
